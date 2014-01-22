81c0f038d37e8214ba4951510d1f2d0ea7098ba3
Fredrik Rubensson <fredrik@eldfluga.se>
2014-01-22 21:17:00 +0100

    moving from stuff to beer

--- anyrest.rb
+++ anyrest.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 require 'json'
+require 'securerandom'
 
-get '/stuff' do
+get '/:resource' do
   [].to_json  
+end
+
+post '/:resource' do
+  SecureRandom.uuid
 end
--- anyrest_test.rb
+++ anyrest_test.rb
@@ -12,9 +12,15 @@ class AnyRestTest < MiniTest::Unit::TestCase
     Sinatra::Application
   end
 
-  def test_getting_empty_list_of_resources
-    get '/stuff'
+  def test_getting_empty_list_of_beer
+    get '/beer'
     assert last_response.ok?
     assert_equal [].to_json, last_response.body
+  end
+
+  def test_posting_some_beer
+    post '/beer', name: 'Chimay Red', alcohol: '12'
+    assert last_response.ok?
+    assert_equal 36, last_response.body.length
   end
 end
