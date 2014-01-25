d5658ef7bb7923f35a0b1c81503e0f421e614454
Fredrik Rubensson <fredrik@eldfluga.se>
2014-01-25 10:01:10 +0100

    reordering and comments

--- anyrest.rb
+++ anyrest.rb
@@ -3,8 +3,14 @@ require 'json'
 require 'securerandom'
 require 'pry'
 
+
+# globals - most importantly the hash containing all them resources
+
 DA_STUFF = {}
 KEY_SEPARATOR = '§'
+
+
+# helper methods to make it easier to do stuff
 
 def find_stuff key
   stuff = DA_STUFF[key]
@@ -23,22 +29,12 @@ def key_from resource, id
   "#{resource}#{KEY_SEPARATOR}#{id}"
 end
 
+
+# operations on a specific resource instance
+
 get '/*/*' do |resource, id|
   key = key_from resource, id
   find_stuff( key ).to_json
-end
-
-get '/*' do |resource|
-  resources = DA_STUFF.select { |k, v| k.start_with? "#{resource}#{KEY_SEPARATOR}" }
-  "#{resources.to_json}\n"
-end
-
-post '/*' do |resource|
-  new_id = SecureRandom.uuid
-  key = key_from resource, new_id
-  clean params
-  DA_STUFF[key] = params
-  new_id
 end
 
 put '/*/*' do |resource, id|
@@ -53,9 +49,28 @@ delete '/*/*' do |resource, id|
   DA_STUFF.delete key
 end
 
+
+# overall operation on a resource
+
+get '/*' do |resource|
+  resources = DA_STUFF.select { |k, v| k.start_with? "#{resource}#{KEY_SEPARATOR}" }
+  "#{resources.to_json}\n"
+end
+
+post '/*' do |resource|
+  new_id = SecureRandom.uuid
+  key = key_from resource, new_id
+  clean params
+  DA_STUFF[key] = params
+  new_id
+end
+
 delete '/*' do |resource|
   DA_STUFF.delete_if { |k, v| k.start_with? "#{resource}#{KEY_SEPARATOR}" }
 end
+
+
+# the root of things
 
 delete '/' do
   DA_STUFF = {}
